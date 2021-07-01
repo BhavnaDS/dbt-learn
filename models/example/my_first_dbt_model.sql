83dcb4562e2e22c02205657eea2ad4103cdd70aa
gupta.bhavna104@gmail.com <gupta.bhavna104@gmail.com>
2021-07-01 06:37:50 +0000

    first_dbt_model_fix

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,6 +7,8 @@
     Try changing "table" to "view" below
 */
 
+/*try saving to git*/
+
 {{ config(materialized='table') }}
 
 with source_data as (
@@ -19,6 +21,7 @@ with source_data as (
 
 select *
 from source_data
+
 
 /*
     Uncomment the line below to remove records with null `id` values
